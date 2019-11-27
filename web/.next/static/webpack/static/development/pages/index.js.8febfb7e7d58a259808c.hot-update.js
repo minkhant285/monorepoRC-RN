85a73963b8f8b207bfc257c25e9eebd8b704d822
minkhant285 <bryyan.mk@gmail.com>
2019-11-27 12:48:00 +0630

    readded web folder

---
+++ web/.next/static/webpack/static/development/pages/index.js.8febfb7e7d58a259808c.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "../shared/http/httpconfig.json":
+/*!**************************************!*\
+  !*** ../shared/http/httpconfig.json ***!
+  \**************************************/
+/*! exports provided: host, port, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"host\":\"192.168.1.2\",\"port\":3000}");
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.8febfb7e7d58a259808c.hot-update.js.map
---
+++ web/pages/index.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+import Nav from '../components/nav'
+import Demo from '../../shared/Demo'
+import { httpGet } from '../../shared/http/httpHandler'
+
+
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       data:'',
+       fatched:false
+    }
+  }
+  
+  getDataFromApi(){
+    httpGet('connectionService/getConnections').then(
+      result =>
+      this.setState({
+        data:result,
+        fatched:true
+      })
+      )
+  }
+
+  componentDidMount(){
+    //this.getDataFromApi()
+  }
+
+  render() {
+      return (
+        <div>
+          <Nav/>
+          <Demo/>
+        </div>
+      )
+    }
+}
+
+export default Home
